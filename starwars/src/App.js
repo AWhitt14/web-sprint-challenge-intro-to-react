08c4b75a31577875ce2edb4b383600c0479679f6
Alex Whitt <whitta14@gmail.com>
2020-06-27 13:31:15 -0400

    rendered from api

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Axios from 'axios';
+import CharacterPan from './character/character';
 
 const App = () => {
+  const [char, setCharacters] = useState([]);
+  useEffect(() => {
+    Axios
+    .get('https://rickandmortyapi.com/api/character')
+    .then(res => {
+      console.log(res.data.results);
+       setCharacters(res.data.results);
+    })
+    .catch(er => {
+      console.log(er);
+    });
+  }, []);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,6 +26,11 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div>
+       { char.map(p => {
+         return <CharacterPan key={p.id} name={p.name}/>
+       })}
+      </div>
     </div>
   );
 }
---
+++ starwars/src/character/character.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CharacterPan = (props) => {
+
+
+    return (
+        <div>
+            <h2>{props.name}</h2>
+        </div>
+
+    );
+};
+
+export default CharacterPan;
